bbe3efdf12185b1693751e975155840f96b8607d
Hamdi Mahamed <hamdim@Hamdis-MacBook-Pro.local>
2022-01-31 13:50:12 -0600

    pseudo stuff

--- lesson-2/pseudo.js
+++ lesson-2/pseudo.js
@@ -58,16 +58,97 @@
 
 //PRINT newArray
 
-let strings = ["push", "to", "from"];
+// let strings = ["push", "to", "from"];
 
-function stringArray(strings) {
-  let newArray = [];
+// function stringArray(strings) {
+//   let newArray = [];
 
-  strings.forEach((string) => {
-    newArray = strings.join();
-  });
+//   strings.forEach((string) => {
+//     newArray = strings.join();
+//   });
 
-  return newArray;
-}
+//   return newArray;
+// }
 
-console.log(stringArray(strings));
+// console.log(stringArray(strings));
+
+//3. a method that takes an array of integers,
+//and returns a new array with every other element
+//from the original array, starting with the
+//first element. For instance:
+
+//everyOther([1,4,7,2,5]); // => [1,7,5]
+
+//Casual Pseudocode
+
+//Make a function that takes the array as an argument
+//Then, set new array equal to empty array
+//Then, iterate through every other element of the array using a for loop
+//(If index of element is divisible by 2, push value onto new Array)
+//Then, push every other element onto the new array
+//Return the new array
+
+// function everyOther(arr) {
+//   let newArr = [];
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (i % 2 === 0) {
+//       newArr.push(arr[i]);
+//     }
+//   }
+
+//   return newArr;
+// }
+
+// console.log(everyOther([1, 4, 7, 2, 5]));
+
+//4. a function that determines the index of
+//the 3rd occurrence of a given character in
+//a string. For instance, if the given character
+//is 'x' and the string is 'axbxcdxex', the
+//function should return 6 (the index of the
+//3rd 'x'). If the given character does not
+//occur at least 3 times, return null.
+
+//Casual pseudocode
+
+//Make a function that takes a string as an argument
+//Set count = 0
+//Then, traverse the string character by character
+//Increment count by 1 if it matches the character
+//If count becomes equal to 3 (occurs 3 times),
+//return latest found index
+//If count does NOT match with 3 after looping,
+// return -1
+
+// function countString(str, letter) {
+//   let count = 0;
+
+//   //loop thorugh to find Nth occurrence of character
+//   for (let i = 0; i < str.length; i++) {
+//     if (str[i] == letter) count += 1;
+
+//     if (count == 3) return i;
+//   }
+//   return -1;
+// }
+
+// console.log(countString("axbxcdxex", "x", 3));
+
+//5. a function that takes two arrays of
+//numbers and returns the result of merging
+//the arrays. The elements of the first array
+// should become the elements at the even indexes
+//of the returned array, while the elements
+//of the second array should becoome the
+//elements at the odd indexes. For instance:
+
+//Maybe
+//1st you can join both arrays
+//And then you sort them:
+//Loop through each array and add to new array
+//based on this:
+//1st array elements added to even index of new array
+//2nd array elements added to odd index of new Array
+
+//
