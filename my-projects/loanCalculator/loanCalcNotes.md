0b0af7ea214cc0c3b4f516a39bbbf23f9ee84a30
Hamdi Mahamed <hamdi.mahamed@hotmail.com>
2022-03-25 01:05:54 -0500

    more loan Calc work

--- my-projects/loanCalculator/loanCalcNotes.md
+++ my-projects/loanCalculator/loanCalcNotes.md
@@ -11,17 +11,33 @@ Output:
 
 Use the following formula
 
-//If amount includes $ sign, then remove it
+If amount includes $ sign, then remove it
 
-//Prompt for annual percentage rate (APR)
-//If APR includes a % sign, then remove it before continuing.
-//Validate if rate is NOT a number (using isNaN() function)
-//No interest loans
+If user enters 'amount' that's not a number,
+then prompt them to enter input again
 
-//if APR is 0, then set monthlyRate = 0;
+Check for 0 and negative numbers for loan amount, APR,
+& loan duration
 
-//Check if loan duration is NOT a number (using isNaN())
-//Check if loan duration is in months or years
+Make sure to set 'amount' to type of 'Number' when passing amount
+as argument to 'invalidAmount() function
+Or else the function will use a string value for amount and it will never exit
 
-//Now cmnvert durationYears to months and add to durationMonths
-//to get loanMonths
+Make sure to change amount to number for 2nd attempt
+after prompting for another attempt
+or else the loop wont exit, since amount is a string still
+and also add logic to remove $ or commas as well since
+that will make amount NaN as well
+
+Prompt for annual percentage rate (APR)
+If APR includes a % sign, then remove it before continuing.
+Validate if rate is NOT a number (using isNaN() function)
+No interest loans
+
+if APR is 0, then set monthlyRate = 0;
+
+Check if loan duration is NOT a number (using isNaN())
+Check if loan duration is in months or years
+
+Now convert durationYears to months and add to durationMonths
+to get loanMonths
--- my-projects/loanCalculator/loanCalculator.js
+++ my-projects/loanCalculator/loanCalculator.js
@@ -4,22 +4,35 @@ function prompt(msg) {
   console.log(`=> ${msg}`);
 }
 
+function invalidAmount(amount) {
+  while (isNaN(amount) || !(amount > 0)) {
+    prompt(
+      "Must enter either a whole number or number up to 2 decimal places."
+    );
+    amount = Number(readline.question());
+  }
+  return amount;
+}
+
 function getLoanAmount(amount) {
   prompt("What's the loan amount?");
   amount = readline.question();
 
-  if (amount.includes("$")) {
-    amount = Number(amount.replaceAll(",", "").replaceAll("$", ""));
-  } else if (!amount.includes("$")) {
-    amount = Number(amount.replaceAll(",", ""));
-  }
+  amount = amount.replaceAll(",", "").replaceAll("$", "");
 
-  while (isNaN(amount)) {
-    prompt("Must enter number for loan amount.");
-    amount = readline.question();
-  }
+  invalidAmount(Number(amount));
 
-  return amount;
+  return Number(amount);
+}
+
+function invalidRate(rate) {
+  while (isNaN(rate) || !(rate > 0)) {
+    prompt(
+      "Must enter either a whole number or number up to 2 decimal places."
+    );
+    rate = readline.question();
+  }
+  return rate;
 }
 
 function getMonthlyRate(rate) {
@@ -32,16 +45,22 @@ function getMonthlyRate(rate) {
     rate = Number(rate.slice(0, -1));
   }
 
-  while (isNaN(rate)) {
-    prompt(
-      "Must enter either a whole number or number up to 2 decimal places."
-    );
-    rate = readline.question();
-  }
+  invalidRate(Number(rate));
 
   rate = Number(rate / 100 / 12);
 
   return rate;
+}
+
+function invalidDuration(years, months) {
+  while (isNaN(years, months) || !((years, months) > 0)) {
+    prompt("Must enter whole number for years and/or months.");
+    prompt("Years:");
+    years = Number(readline.question()) * 12;
+    prompt("Months:");
+    months = Number(readline.question());
+  }
+  console.log(years, months);
 }
 
 function getLoanDuration(years, months) {
@@ -52,23 +71,21 @@ function getLoanDuration(years, months) {
   prompt("Months:");
   months = Number(readline.question());
 
-  while (isNaN(years, months)) {
-    prompt("Must enter whole number for years and/or months.");
-    years = Number(readline.question());
-    months = Number(readline.question());
-  }
+  invalidDuration(Number(years), Number(months));
 
   return years + months;
 }
 
 function getMonthlyPay(loanAmount, monthlyRate, loanMonths) {
   let payment;
+
   if (monthlyRate === 0) {
     payment = loanAmount / loanMonths;
   } else {
     payment =
       loanAmount * (monthlyRate / (1 - Math.pow(1 + monthlyRate, -loanMonths)));
   }
+
   return payment.toFixed(2);
 }
 
